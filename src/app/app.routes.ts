94032d511ef5e44fb7e2ea67b68ff92875f3233b
Jorge Fernandez <jorgejgdrj41@gmail.com>
2024-07-13 00:39:44 -0500

    Add base layout and home page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: HomePageComponent,
+      }
+    ]
+  },
+  {
+    path: "**",
+    redirectTo: "",
+  }
+];
---
+++ src/app/layout/base-layout/base-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-base-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './base-layout.component.html',
+  styleUrl: './base-layout.component.scss'
+})
+export class BaseLayoutComponent {
+
+}
