d43817d5b21945d4253f6e4d442ee2c350cf2f68
Jorge Fernandez <jorgejgdrj41@gmail.com>
2024-07-14 01:19:40 -0500

    Update contact page with details

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,12 +1,32 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { SectionTitleComponent } from '../../components/section-title/section-title.component';
 
+const ICON_CLASSNAME: string = " hover:text-primary transition-duration-400";
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgClass, SectionTitleComponent],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+  email: string = "jorgejgdrj41@gmail.com";
+  itchioUrl: string = "https://drande.itch.io/";
 
+  contactItems: MenuItem[] = [
+    {
+      icon: PrimeIcons.WHATSAPP.concat(ICON_CLASSNAME),
+      url: "https://wa.me/573118683827"
+    },
+    {
+      icon: PrimeIcons.LINKEDIN.concat(ICON_CLASSNAME),
+      url: "https://www.linkedin.com/in/jorge-fernandez-fullstack-unity-developer/"
+    },
+    {
+      icon: PrimeIcons.GITHUB.concat(ICON_CLASSNAME),
+      url: "https://github.com/Drande"
+    },
+  ];
 }
