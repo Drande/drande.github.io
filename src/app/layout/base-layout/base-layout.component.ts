82cb087d754327fbecf2cdfc6dc02cd01c18a9c7
Jorge Fernandez <jorgejgdrj41@gmail.com>
2024-07-14 01:43:06 -0500

    Update base layout page with navigations and footer

--- src/app/layout/base-layout/base-layout.component.ts
+++ src/app/layout/base-layout/base-layout.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { NgClass } from '@angular/common';
+import { ThemeTogglerComponent } from '../../components/theme-toggler/theme-toggler.component';
 
 @Component({
   selector: 'app-base-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MenubarModule, NgClass, ThemeTogglerComponent],
   templateUrl: './base-layout.component.html',
   styleUrl: './base-layout.component.scss'
 })
 export class BaseLayoutComponent {
+  items: MenuItem[] = [
+    {
+      label: "Home",
+      icon: PrimeIcons.HOME,
+      routerLink: "/",
+      fragment: "home",
+    },
+    {
+      label: "Proyects",
+      icon: PrimeIcons.FILE,
+      routerLink: "/",
+      fragment: "proyects",
+    },
+    {
+      label: "About me",
+      icon: PrimeIcons.USER,
+      routerLink: "/",
+      fragment: "about",
+    },
+    {
+      label: "Resume",
+      icon: PrimeIcons.FILE,
+      routerLink: "/resume",
+    },
+    {
+      label: "Contact",
+      routerLink: "/",
+      icon: PrimeIcons.PHONE,
+      fragment: "contact",
+    }
+  ];
 
 }
